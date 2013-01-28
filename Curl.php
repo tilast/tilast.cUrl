f2e0924d89949d1c43e6f16c4348b19933801339
tilast <tilast.lab@gmail.com>
2013-01-28 23:00:42 +0200

    Smth new

--- Curl.php
+++ Curl.php
@@ -14,31 +14,103 @@ class Curl
 
     /**
      * Constructor
-     * @param json-string with the curl parametrs $params
+     * parameters you must write without "CURLOPT_"-prefix
+     * @param json-string or array with the curl parameters $params
+     * example of json-string: '{"url":"http://google.com.ua/"}'
+     * example of array: array("url" => "http://google.com.ua/")
      */
-    function __construct($params)
+    function __construct($params = null)
     {
         // initialize of curl
-        $this->handler = curl_init();
-        if(!$this->handler)
+        if(!function_exists("curl_init"))
             throw new CurlInitException("Curl is not required to your server");
 
+        // create handler of this curl connection
+        $this->handler = curl_init();
+        if(!$this->handler)
+            throw new CurlInitException("Can't create handler of curl connection");
+
+        if($params)
+            $this->setParams($params);
+    }
+
+
+    /**
+     * Sets parameters of curl query
+     * parameters you must write without "CURLOPT_"-prefix
+     * @param json-string or array with the curl parameters $params
+     * example of json-string: '{"url":"http://google.com.ua/"}'
+     * example of array: array("url" => "http://google.com.ua/")
+     */
+    public function setParams($params)
+    {
+
+        // checking of input data
+        if(!$params)
+            throw new CurlParamsException("An array of parameters is empty");
+
+        // processing of input data
+        $paramsArray = (is_array($params)) ? $params : json_decode($params, true);
+        // array for wrong parameters, if they'll be exist
+        $wrongParams = array();
+
         // try getting parameters
-        $paramsArray = json_decode($params, true);
         foreach($paramsArray as $key => $value)
         {
-            // create the new exception if there is a outside parameter
+
+            // write the wrong parameter into $wrongParams
             if(!defined("CURLOPT_".strtoupper($key)))
-                throw new CurlParamsException("Such parameter is not exist");
+            {
+                $wrongParams[] = $key;
+                continue;
+            }
 
             // creating of array for curl_setopt_array
             $paramsArray[constant("CURLOPT_".strtoupper($key))] = $value;
-            // deleting of extra cells of array
+
+            // Remove unnecessary elements of the array. This string is important, because $paramsArray must have data for curl_setopt_array() function only
             unset($paramsArray[$key]);
+
         }
+
+        // throwing of wrong parameters
+        if(!empty($wrongParams))
+        {
+            $keys = implode(", ", $wrongParams);
+            throw new CurlParamsException("parameter <b style='color: red'>".$keys."</b> is not exist");
+        }
+
+        // setting default parameters
+        $paramsArray[constant("CURLOPT_RETURNTRANSFER")] = "1";
+        $paramsArray[constant("CURLOPT_USERAGENT")] = "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.56 Safari/537.17"; // will be changed
 
         // setting of parameters
         curl_setopt_array($this->handler, $paramsArray);
+
+    }
+
+
+    /**
+     * @param $postArray - array for the POST-query
+     * Method adds the post-query to this curl connection
+     */
+    public function setPost($postArray)
+    {
+        // check input data
+        if(empty($postArray))
+            throw new CurlPostException("post array is empty");
+        else
+        {
+            // create th post-query string
+            $firstKey = key($postArray);
+            $postStr = "";
+            foreach($postArray as $key => $value)
+            {
+                $postStr .= ($firstKey == $key) ? $key."=".$value : "&".$key."=".$value;
+            }
+            // set parameter
+            curl_setopt($this->handler, CURLOPT_POSTFIELDS, $postStr);
+        }
     }
 
     /**
@@ -97,22 +169,31 @@ class CurlExecException extends CurlException
 {
     function __construct($message)
     {
-        parent::__construct("Exec Exception, ".$message);
+        parent::__construct("Execution Exception, ".$message);
+    }
+}
+class CurlPostException extends CurlException
+{
+    function __construct($message)
+    {
+        parent::__construct("Creating POST-query Exception, ".$message);
     }
 }
 
 /* Маленький пример работы */
 
-try
+/*try
 {
-$curl = new Curl
-('{
-    "url":"http://localhost/ukraine/",
-    "returntransfer": true
-}');
-echo $curl->exec();
+    $curl = new Curl();
+    $curl->setParams
+    (
+        //'{"url":"http://google.com.ua/"}'
+        array("url" => "http://google.com.ua/", "post" => "1")
+    );
+    $curl->setPost(array("name" => "Ihor", "surname" => "Kroosh"));
+    echo $curl->exec();
 }
 catch(CurlException $e)
 {
     echo $e->getMessage();
-}
+}*/
